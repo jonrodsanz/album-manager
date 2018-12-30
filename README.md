b4749dd6e53570b76f0898dd437e2470c24b4d71
Jonathan Rodriguez <jonrodsanz@hotmail.com>
2018-12-29 18:14:28 -0600

    Add README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+<center>
+  <img height="50px" src="./public/assets/Musiger.png" />
+  <h1>Album Manager</h1>
+  <p>This is a learning based project built with React and Redux.</p>
+</center>
+
+---
+## Installation
+To run locally type the following command
+
+> you must have [npm](https://nodejs.orgs) installed
+
+``
+  npm install
+``
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,20 @@
 import React from "react";
 
+//http://icons.iconarchive.com/icons/raindropmemory/laboratory/512/Art-icon.png
+
 function Navbar() {
   return (
     <div style={{backgroundColor: "#2d2d2d", opacity: 0.93}}>
       <nav className="navbar navbar-dark container">
         <a className="navbar-brand" href="#">
           <img
-            src="http://icons.iconarchive.com/icons/raindropmemory/laboratory/512/Art-icon.png"
-            width="30"
-            height="30"
+            src="../assets/Musiger.png"
+            width="100"
+            height="35"
             style={{ marginRight: 15 }}
-            class="d-inline-block align-top"
+            className="d-inline-block align-top my-0"
             alt="Logo"
           />
-          Music Collection
         </a>
       </nav>
     </div>
